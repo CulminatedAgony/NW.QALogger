c79de14fb05e64642d8a7860214a86ece8c8d233
Freaky <90410500+CulminatedAgony@users.noreply.github.com>
2023-08-31 06:40:27 -0700

    New

--- NW.QALogger/Form1.Designer.cs
+++ NW.QALogger/Form1.Designer.cs
@@ -28,18 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            listBox1 = new ListBox();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Segoe UI Emoji", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Snow;
+            label1.Location = new Point(27, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(303, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome to QA Logger!";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            label1.Click += label1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = SystemColors.InfoText;
+            listBox1.Font = new Font("Segoe UI Emoji", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            listBox1.ForeColor = Color.White;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 26;
+            listBox1.Items.AddRange(new object[] { "confirmed-bugs", "cannot-reproduce", "uncertain-if-bug", "event-bugs", "map-generator-bugs", "not-scpsl-bugs", "duplicates", "old-bugs", "fixed", "unity-issues", "crash-reports" });
+            listBox1.Location = new Point(27, 72);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(199, 82);
+            listBox1.TabIndex = 1;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox1.ForeColor = Color.Chartreuse;
+            checkBox1.Location = new Point(251, 81);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(52, 25);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "✅";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            checkBox2.ForeColor = Color.Red;
+            checkBox2.Location = new Point(251, 112);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(52, 25);
+            checkBox2.TabIndex = 2;
+            checkBox2.Text = "❌";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(27, 213);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(303, 23);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI Emoji", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Snow;
+            label2.Location = new Point(12, 189);
+            label2.Name = "label2";
+            label2.Size = new Size(150, 21);
+            label2.TabIndex = 0;
+            label2.Text = "Link to message";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            label2.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(127, 258);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Send";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.InfoText;
+            BackgroundImageLayout = ImageLayout.Center;
             ClientSize = new Size(360, 450);
-            Name = "QA Logger";
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(listBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
             Text = "QA Logger";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private ListBox listBox1;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button1;
     }
 }
--- NW.QALogger/Form1.cs
+++ NW.QALogger/Form1.cs
@@ -1,10 +1,150 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Http;
+using System.Text;
+using System.Windows.Forms;
+
 namespace NW.QALogger
 {
     public partial class Form1 : Form
     {
+        private System.Windows.Forms.Timer timer;
+        private Random random;
+        private Color targetColor;
+        private int step = 5;
+        private string selectedItem;
+
         public Form1()
         {
             InitializeComponent();
+
+            random = new Random();
+            targetColor = GenerateRandomColor();
+
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 50;
+            timer.Tick += Timer_Tick;
+
+            listBox1.SelectionMode = SelectionMode.One;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Color currentColor = label1.ForeColor;
+            int r = MoveTowards(currentColor.R, targetColor.R);
+            int g = MoveTowards(currentColor.G, targetColor.G);
+            int b = MoveTowards(currentColor.B, targetColor.B);
+
+            label1.ForeColor = Color.FromArgb(r, g, b);
+
+            if (currentColor == targetColor)
+            {
+                targetColor = GenerateRandomColor();
+            }
+        }
+
+        private Color GenerateRandomColor()
+        {
+            return Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+        }
+
+        private int MoveTowards(int current, int target)
+        {
+            if (current < target)
+            {
+                return Math.Min(current + step, target);
+            }
+            else if (current > target)
+            {
+                return Math.Max(current - step, target);
+            }
+            else
+            {
+                return current;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                selectedItem = listBox1.SelectedItem.ToString();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked && checkBox2.Checked)
+            {
+                MessageBox.Show("Please select only one option.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                checkBox1.Checked = false;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked && checkBox2.Checked)
+            {
+                MessageBox.Show("Please select only one option.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                checkBox2.Checked = false;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (selectedItem == null)
+            {
+                MessageBox.Show("Please select an item from the list.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Please enter a message in the text box.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string webhookUrl = "your_webhook_url_here";
+
+            string reaction = checkBox1.Checked ? "✅" : (checkBox2.Checked ? "❌" : "");
+            string content = JsonConvert.SerializeObject(new
+            {
+                content = " ",
+                embeds = new[]
+                {
+            new
+            {
+                title = "New Message",
+                description = $"Channel: {selectedItem}\nMessage: {textBox1.Text}\nReacted: {reaction}",
+                color = 0xFF0000,
+            }
+        }
+            });
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpContent httpContent = new StringContent(content, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(webhookUrl, httpContent);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Message sent successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Error sending message: " + response.ReasonPhrase, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
